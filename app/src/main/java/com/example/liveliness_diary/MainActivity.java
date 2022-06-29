1ae2e3f0273817e12334e032a236d107c27268ea
yoon-seo-yeon <s2013@e-mirim.hs.kr>
2022-06-29 21:17:27 +0900

    Initial commit

---
+++ app/src/main/java/com/example/liveliness_diary/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.liveliness_diary;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
